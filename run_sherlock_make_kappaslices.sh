caf0675c114bcd276a72d35ddb944ee01b57e798
Yuuki Omori <yomori@sh02-ln02.stanford.edu>
2020-07-22 09:12:18 -0700

    update

--- run_sherlock_make_kappaslices.sh
+++ run_sherlock_make_kappaslices.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+dir_in=/home/users/yomori/oak/mdpl2/dens/
+dir_out=/scratch/users/yomori/raytrace2/
+cambpar=mdpl2_params.ini
 echo '#!/bin/bash' > submit_job
-echo "#SBATCH -t 2:00:00">> submit_job
-echo "#SBATCH --partition=broadwl" >> submit_job
-echo "#SBATCH --exclusive" >> submit_job
+echo "#SBATCH -t 24:00:00">> submit_job
+echo "#SBATCH --partition=kipac" >> submit_job
+echo "#SBATCH -c 4" >> submit_job
 echo "#SBATCH --nodes=1" >> submit_job
 echo "#SBATCH --job-name=heal2psi${i}" >> submit_job
-#echo "#SBATCH --array=5-6" >> submit_job
-echo "#SBATCH --mem=42GB">> submit_job
-#echo "#SBATCH --begin=04:00:00" >> submit_job
+echo "#SBATCH --mem=60GB">> submit_job
 echo "cd $PWD" >> submit_job
-echo "export OMP_NUM_THREADS=24" >> submit_job
-echo "source /project2/chihway/setup/setup_intel.sh" >> submit_job
+echo "export OMP_NUM_THREADS=4" >> submit_job
+echo "source /home/groups/kipac/yomori/setup.sh" >> submit_job
 echo  make_densityfiles.py ${dir_in} ${dir_out} ${cambpar} >> submit_job
 sbatch submit_job
